8834f2be01ee3233b8d06ef0e6865d0d89b9f337
kbuhantsev <k.buhantsev@gmail.com>
2024-03-05 18:14:46 +0200

    fix creation_date

--- scarping_learning/dom_ria.py
+++ scarping_learning/dom_ria.py
@@ -3,24 +3,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from selenium import webdriver
+from fake_useragent import UserAgent
+
 
 def main():
-    url = "https://dom.ria.com/uk/search/?excludeSold=1&category=1&realty_type=2&operation=1&state_id=12&price_cur=1\
-    &wo_dupl=1&sort=inspected_sort&period=per_allday&firstIteraction=false&city_ids=12&client=searchV2&limit=20\
-    &type=list&ch=242_239,247_252,1437_1436"
+    url = "https://dom.ria.com/uk/search/?excludeSold=1&category=1&realty_type=2&operation=1&state_id=12&price_cur=1&wo_dupl=1&sort=inspected_sort&period=per_allday&firstIteraction=false&city_ids=12&client=searchV2&limit=20&type=list&ch=242_239,247_252,1437_1436"
+
+    user_agent = UserAgent()
 
     service = Service(ChromeDriverManager().install())
 
-    options = ChromeOptions()
-    options.add_argument("headless")
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    # options.add_argument("--ignore-certificate-errors")
+    # options.add_argument("--incognito")
+    options.add_argument("--headless")
+    # options.add_argument(f"--proxy-server=5.189.158.162:3128")
+    options.add_argument(f"user-agent={user_agent.random}")
 
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option("useAutomationExtension", False)
-
-    driver = Chrome(service=service, options=options)
-
-    driver.maximize_window()
+    driver = webdriver.Chrome(service=service, options=options)
 
     driver.get(url)
     driver.implicitly_wait(0.5)
