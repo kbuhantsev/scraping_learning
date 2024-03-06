6d62966844bc565d1b8acb300bcfd48887411e38
kbuhantsev <k.buhantsev@gmail.com>
2024-03-06 18:27:59 +0200

    fix creation_date

--- scarping_learning/dom_ria.py
+++ scarping_learning/dom_ria.py
@@ -1,21 +1,23 @@
-# from selenium.webdriver import Keys, ChromeOptions, Chrome
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-
 # from selenium import webdriver
-# from seleniumwire import webdriver
+import json
+
+from selenium_stealth import stealth
 from fake_useragent import UserAgent
 
-# import seleniumwire.undetected_chromedriver as uc
+from seleniumwire import webdriver
 
-
-from selenium import webdriver
-from selenium_stealth import stealth
 import time
+import gzip
 
 
-def main():
+def my_response_interceptor(request, response):
+    if request.url.startswith("https://dom.ria.com/realty/data/"):
+        # print(request.url, response.body.decode("utf-8"))
+        body = gzip.decompress(response.body)
+        print(body.decode("utf-8"))
+
+
+def get_page_soup():
 
     options = webdriver.ChromeOptions()
     options.add_argument("start-maximized")
@@ -40,8 +42,17 @@ def main():
         fix_hairline=True,
     )
 
-    url = "https://dom.ria.com/uk/search/?excludeSold=1&category=1&realty_type=2&operation=1&state_id=12&price_cur=1&wo_dupl=1&sort=inspected_sort&period=per_allday&firstIteraction=false&city_ids=12&client=searchV2&limit=20&type=list&ch=242_239,247_252,1437_1436"
+    url = "https://dom.ria.com/uk/search/?excludeSold=1&category=1&realty_type=2&operation=1&state_id=12&price_cur=1&wo_dupl=1&sort=inspected_sort&period=per_allday&firstIteraction=false&city_ids=12&client=searchV2&limit=20&type=list&ch=226_223,246_244,1437_1436%3A"
+
+    driver.response_interceptor = my_response_interceptor
+
     driver.get(url)
+
+    # for request in driver.requests:
+    #     if request.response:
+    #         if request.url.startswith("https://dom.ria.com/realty/data/"):
+    #             print(request.url, json.loads(request.response.body))
+
     time.sleep(5)
 
     src = driver.page_source
@@ -51,31 +62,12 @@ def main():
     driver.quit()
 
 
-# def main():
-#     url = "https://dom.ria.com/uk/search/?excludeSold=1&category=1&realty_type=2&operation=1&state_id=12&price_cur=1&wo_dupl=1&sort=inspected_sort&period=per_allday&firstIteraction=false&city_ids=12&client=searchV2&limit=20&type=list&ch=242_239,247_252,1437_1436"
-#
-#     user_agent = UserAgent()
-#
-#     options = webdriver.ChromeOptions()
-#     # options.add_argument("start-maximized")
-#     # options.add_argument("--ignore-certificate-errors")
-#     # options.add_argument("--incognito")
-#     options.add_argument("--headless")
-#     # options.add_argument(f"--proxy-server=5.189.158.162:3128")
-#     options.add_argument(f"user-agent={user_agent.random}")
-#
-#     driver = webdriver.Chrome(
-#         options=options,
-#         seleniumwire_options={"disable_capture": True, "request_storage": "memory"},
-#     )
-#
-#     driver.get(url)
-#     driver.implicitly_wait(0.5)
-#
-#     src = driver.page_source
-#     with open("data.html", "w", encoding="utf-8") as file:
-#         file.write(src)
-
-
 if __name__ == "__main__":
-    main()
+
+    cities = []
+
+    # realty_type 2 - квартиры, 0 - дома
+    operations = [1, 3]  # operation 1 - продажа, 3 - аренда
+    categories = [1, 4]  # category 1 - квартиры, 4 - дома
+
+    get_page_soup()
